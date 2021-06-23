c127ca329a94572c46bea50a97b2f83e35844255
Viktor Likhanov <volixanov@unitbean.io>
2021-06-23 12:13:29 +0300

    refactor: updated dependencies repo resolving strategy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,13 @@
 buildscript {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.1")
         classpath(kotlin("gradle-plugin", version = "1.5.10"))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.15.1")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        mavenCentral()
     }
 }
 
